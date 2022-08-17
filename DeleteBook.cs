4fcb02d097ac4840b026fcdca835bd98b13156bd
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-17 17:56:54 +0530

    Added DeleteBook class

---
+++ DeleteBook.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBook
+{
+    public class DeleteBook : EditBook
+    {
+        public void Delete()
+        {
+            Console.Write("\nEnter the name of person to delete : "); var dictionaryKey = Console.ReadLine().ToUpper();
+            BookDictionary.Remove(dictionaryKey);
+        }
+    }
+    
+}
