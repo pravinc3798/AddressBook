f88fd61c2f1d30d310686e97b37cb5620a2f3e14
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-17 17:57:12 +0530

    Added ViewBook class

---
+++ ViewBook.cs
@@ -0,0 +1,26 @@
+﻿namespace AddressBook
+{
+    public class ViewBook : DeleteBook
+    {
+        public void ViewNames()
+        {
+            Console.WriteLine("\nAddress Book Contains following members : ");
+
+            foreach (var item in BookDictionary.Keys)
+            {
+                Console.WriteLine(" [+] Name : {0}", item);
+            }
+        }
+
+        public void ViewDetails()
+        {
+            Console.Write("\n  Enter the name of member to view details : "); var dictionaryKey = Console.ReadLine().ToUpper();
+
+            foreach (var item in BookDictionary[dictionaryKey])
+            {
+                Console.WriteLine(" >> {0}", item);
+            }
+        }
+    }
+    
+}
