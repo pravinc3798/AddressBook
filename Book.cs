15c8739bd17d63672b59ab7f8a54cda2b9c6dcc6
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-17 17:56:48 +0530

    Added Book class

---
+++ Book.cs
@@ -0,0 +1,23 @@
+﻿namespace AddressBook
+{
+    public class Book
+    {
+        public Dictionary<string, List<string>> BookDictionary = new Dictionary<string, List<string>>();
+
+        public void Add()
+        {
+            Console.Write("\n [+] Enter Full Name : "); var name = Console.ReadLine();
+            Console.Write(" [+] Enter Address : "); var address = Console.ReadLine();
+            Console.Write(" [+] Enter City : "); var city = Console.ReadLine();
+            Console.Write(" [+] Enter State : "); var state = Console.ReadLine();
+            Console.Write(" [+] Enter Zip : "); var zip = Console.ReadLine();
+            Console.Write(" [+] Enter Phone Number : "); var phoneNumber = Console.ReadLine();
+            Console.Write(" [+] Enter Email : "); var email = Console.ReadLine();
+
+            var detailList = new List<string> { address, city, state, zip, phoneNumber, email };
+
+            BookDictionary.Add(name.ToUpper(), detailList);
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Address Book !!");
+
+            // Dummy DATA
+            var check = new SelectionBook();
+            check.BookDictionary.Add("ABC", new List<string> { "Koregoan Park", "Pune", "Maharashtra", "400000", "0122456789", "abc@email.com" });
+            check.BookDictionary.Add("XYZ", new List<string> { "Shivaji Nagar", "Pune", "Maharashtra", "411111", "0456789123", "xyz@email.com" });
+            check.Choose();
+        }
+    }
+
+}
